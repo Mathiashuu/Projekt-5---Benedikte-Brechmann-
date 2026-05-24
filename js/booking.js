5528958f8e8f31ef054d7dbbfa800f0f736c6be2
lauhofn-mmd <lauhofn@gmail.com>
2026-05-24 17:15:23 +0200

    Link til JavaScript

--- js/booking.js
+++ js/booking.js
@@ -1,2 +1,23 @@
 
 // JavaScript til Booking side - lavet af Lau
+// Inspiration fra W3Schools | How TO - CSS/JS Modal
+
+// 'datoBtn' ID til dato select knap
+// 'datoModal' ID til dato modal container
+
+// 'tidBtn'  ID til tid select knap 
+// 'tidModal' ID til tid modal container
+
+// 'closeModal' class til close knap
+
+
+let datoModal = document.getElementById("datoModal");  
+
+let datoOpn = document.getElementById("datoBtn");  
+
+let close = document.getElementsByClassName("closeModal");
+
+
+datoOpn.onclick = function() {
+    datoModal.style.display = "block";
+}
