c9a55bfe17747974d4adff920d3f4c786fe6c8d9
lauhofn-mmd <lauhofn@gmail.com>
2026-05-25 16:18:36 +0200

    Klargøring JS Onboarding

--- js/onboarding.js
+++ js/onboarding.js
@@ -1,3 +1,8 @@
-
 // JavaScript til onboarding side - lavet af Mie + Lau
-// med inspiration fra www.geeksforgeeks.org/ change image and text
+// med inspiration fra W3Schools - Slideshows
+
+// Hvis du ikke kan finde nogle elementer, så prøv at søg i dokumentet med CTRL + F / COMMAND + F (tror jeg)
+
+// "slideshow--container" class til hele slideshow - linje 35 i Onboarding HTML
+// "mySlides"  class til et slide i slideshowet
+// "slideshow--next" class til 'videre' knap
